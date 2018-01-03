304af9466df8a28f10eda52bc8e594afec8af96c
Ivan Milincic <lokomotivan@gmail.com>
2018-01-03 15:03:15 +0100

    if shorhand and examples

--- 1-Introducing the JavaScript Language.js
+++ 1-Introducing the JavaScript Language.js
@@ -170,11 +170,43 @@ var answer = window.prompt("Type YES, NO or MAYBE. Then click OK.");
 if (answer === "YES") {
     console.log("you said YES :)");
 } else if (answer === "MAYBE") {
-    console.log("you said MAYBE :|");
+    console.log("you said MAYBE :| ");
 } else if (answer === "NO") {
     console.log("you said NO :(");
 } else {
     console.log("Hey! Are you messing with me?");
+}
+
+/**
+ *  if Shorthand
+ *
+ */
+var foo = "js";
+if (foo === "js") console.log("I love JavaScript");
+// or
+var animal = "cat";
+(animal === "cat") ? console.log("Cat") : console.log("Dog");
+// or
+var pet = (animal === "cat") ? "my cat" : "my dog";
+
+/**
+ *  if-else @example
+ *
+ */
+var whatYouWant = "puppy";
+var whatYouGot = "puppy";
+var cryAboutIt = false;
+
+if (whatYouWant === whatYouGot) {
+    console.log("O yeah, im so happy!");
+    cryAboutIt = false;
+} else {
+    console.log("Oh no, im sad!");
+    cryAboutIt = true;
+}
+
+if (cryAboutIt) {
+    console.log("Im crying waaaaaaaaaaaaaaaaa")
 }
 
 /*-------------------------------------------------------------
